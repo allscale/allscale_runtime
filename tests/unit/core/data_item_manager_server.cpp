a4bb4869489e8ffacf2832f78b6804e7523b5092
Arne Hendricks <arne.hendricks@fau.de>
2016-11-08 16:03:32 +0100

    working on data_item_manager_server stuff

--- tests/unit/core/data_item_manager_server.cpp
+++ tests/unit/core/data_item_manager_server.cpp
@@ -16,20 +16,21 @@
 #include <hpx/util/high_resolution_timer.hpp>
 
 
-
 typedef allscale::region<int> my_region;
 typedef allscale::data_item_description<my_region,int,int> descr;
 typedef allscale::data_item<descr> test_item;
-
-ALLSCALE_REGISTER_DATA_ITEM_TYPE(descr);
-
+typedef allscale::requirement<descr> test_requirement;
 
 typedef allscale::data_item_manager_server my_data_item_manager_server;
-
 typedef std::pair<hpx::naming::id_type, my_data_item_manager_server> loc_server_pair;
 
 
+ALLSCALE_REGISTER_DATA_ITEM_TYPE(descr);
 ALLSCALE_REGISTER_DATA_ITEM_MANAGER_SERVER_COMPONENT();
+
+
+std::vector< loc_server_pair > dms;
+
 /*
 template<typename T>
 bool check(T first)
@@ -46,33 +47,70 @@ bool check(T first)
 }
 */
 
-
-int hpx_main(int argc, char* argv[])
-{
-
-    std::vector< loc_server_pair > dms;
+bool test_creation_of_data_item_manager_server_components(){
     std::vector<hpx::naming::id_type> localities = hpx::find_all_localities();
     if(hpx::get_locality_id() == 0){
 
         for(hpx::naming::id_type const& node : localities) {
-            std::cout << "Cycling thru localities, Locality: " << node << std::endl;
+            //std::cout << "Cycling thru localities, Locality: " << node << std::endl;
             auto tmp = my_data_item_manager_server(node);
             dms.push_back(std::make_pair(node, tmp));
-
         }
+    }
+    return true;
+}
 
 
-
-        for(loc_server_pair & server_entry : dms)
-        {
-
-            my_region test_region;
-            descr test_descr;
+bool test_creation_of_data_items()
+{
+    for(loc_server_pair & server_entry : dms)
+    {
+        my_region test_region;
+        descr test_descr;
+        for(int i = 0; i < 10; ++i){
             auto test = server_entry.second.create_data_item<descr>(test_descr);
-            std::cout<<"test item global id: " << test.get_gid() << std::endl;
-
+             //std::cout<<"test item global id: " << test.get_gid() << std::endl;
         }
+    }
+    return true;
+}
 
+
+
+
+bool test_locate_method()
+{
+    test_requirement my_req;
+    descr test_descr;
+    for(loc_server_pair & server_entry : dms)
+    {
+        server_entry.second.locate<descr>(my_req);
+    }
+    return true;
+}
+
+
+
+
+
+
+
+
+
+
+
+int hpx_main(int argc, char* argv[])
+{
+    if(test_creation_of_data_item_manager_server_components()==true){
+        std::cout<< "Test(1): Creation of data_item_manager_servers : PASSED"<<std::endl; 
+    }
+ 
+    if(test_creation_of_data_items()==true){
+        std::cout<< "Test(2): Creation of data_items : PASSED"<<std::endl; 
+    }
+    
+    if(test_locate_method()==true){
+        std::cout<< "Test(3): Locate all data_items related to a region of a requirement: PASSED"<<std::endl; 
     }
 
 
