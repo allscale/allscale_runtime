2daf4240802660884d31cadbb077ea7b6c6b5b40
Herbert Jordan <herbert@dps.uibk.ac.at>
2016-11-29 14:55:14 +0100

    Added generic treeture combine operator.

--- allscale/runtime.hpp
+++ allscale/runtime.hpp
@@ -9,8 +9,9 @@
  */
 
 
-#include <allscale/work_item_description.hpp>
+#include <allscale/no_split.hpp>
 #include <allscale/spawn.hpp>
+#include <allscale/work_item_description.hpp>
 
 #include <hpx/hpx_main.hpp>
 
@@ -38,6 +39,47 @@ int main_wrapper(const Args& ... args) {
     // return result (only id == 0 will actually)
     return res;
 }
+
+
+// ---- a generic treeture combine operator ----
+
+struct combine_name {
+    static const char *name() {
+        return "combine";
+    }
+};
+
+template<typename A, typename B, typename Op>
+struct combine_operation {
+
+    static constexpr bool valid = true;
+    using result_type = std::result_of_t<Op(A,B)>;
+
+    // Just perform the addition, no extra tasks are spawned
+    template <typename Closure>
+    static allscale::treeture<result_type> execute(Closure const& closure) {
+        return
+            allscale::treeture<result_type>{
+                Op()(hpx::util::get<0>(closure),hpx::util::get<1>(closure))
+            };
+    }
+};
+
+template<typename A, typename B, typename Op>
+using combine = 
+    allscale::work_item_description<
+        std::result_of_t<Op(A,B)>,
+        combine_name,
+        allscale::no_split<std::int64_t>,
+        combine_operation<A,B,Op>
+    >;
+
+
+template<typename A, typename B, typename Op, typename R = std::result_of_t<Op(A,B)>>
+allscale::treeture<R> treeture_combine(const allscale::treeture<A>&& a, const allscale::treeture<B>& b, const Op&) {
+    return allscale::spawn<combine<A,B,Op>>(a,b);
+}
+
 
 } // end namespace runtime
 } // end namespace allscale
