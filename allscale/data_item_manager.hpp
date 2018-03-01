a841832c0f04dde3b8fab7dc08f84ac121bfd584
Thomas Heller <thomas.heller@cs.fau.de>
2018-03-01 13:14:55 +0100

    Adding first touch on data_item_manager::get

--- allscale/data_item_manager.hpp
+++ allscale/data_item_manager.hpp
@@ -3,6 +3,7 @@
 
 #include <allscale/data_item_manager/acquire.hpp>
 #include <allscale/data_item_manager/data_item_store.hpp>
+#include <allscale/data_item_manager/shared_data.hpp>
 #include <allscale/data_item_reference.hpp>
 #include <allscale/data_item_requirement.hpp>
 #include <allscale/lease.hpp>
@@ -14,33 +15,50 @@
 #include <type_traits>
 
 namespace allscale { namespace data_item_manager {
-        template <typename DataItemType, typename...Args>
-        allscale::data_item_reference<DataItemType>
+        template <typename DataItem, typename...Args>
+        allscale::data_item_reference<DataItem>
         create(Args&&...args)
         {
-            typedef typename DataItemType::shared_data_type shared_data_type;
+            typedef typename DataItem::shared_data_type shared_data_type;
 
-            auto ref = data_item_reference<DataItemType>(
+            auto ref = data_item_reference<DataItem>(
                     hpx::local_new<allscale::detail::id_holder>(
                         [](hpx::naming::gid_type const& id)
                         {
-//                             data_item_manager_impl<DataItemType>::get_ptr()->destroy(id);
+//                             data_item_manager_impl<DataItem>::get_ptr()->destroy(id);
                         }
                     )
                 );
-            auto &item = data_item_store<DataItemType>::lookup(ref);
+            auto &item = data_item_store<DataItem>::lookup(ref);
             item.shared_data.reset(new shared_data_type(std::forward<Args>(args)...));
             return ref;
         }
 
-        template<typename DataItemType>
-        typename DataItemType::facade_type
-        get(const allscale::data_item_reference<DataItemType>& ref)
+        template<typename DataItem>
+        typename DataItem::facade_type
+        get(const allscale::data_item_reference<DataItem>& ref)
         {
             if (ref.fragment == nullptr)
             {
-                auto &item = data_item_store<DataItemType>::lookup(ref);
-                HPX_ASSERT(item.fragment);
+                using mutex_type = typename data_item_store<DataItem>::data_item_type::mutex_type;
+                auto &item = data_item_store<DataItem>::lookup(ref);
+                {
+                    std::unique_lock<mutex_type> l(item.mtx);
+                    if (item.fragment == nullptr)
+                    {
+                        typename DataItem::shared_data_type shared_data_;
+                        {
+                            hpx::util::unlock_guard<std::unique_lock<mutex_type>> ul(l);
+                            shared_data_ = shared_data(ref);
+                        }
+                        if (item.fragment == nullptr)
+                        {
+                            using fragment_type = typename DataItem::fragment_type;
+                            item.fragment.reset(new fragment_type(std::move(shared_data_)));
+                        }
+                    }
+                    HPX_ASSERT(item.fragment);
+                }
                 ref.fragment = item.fragment.get();
             }
 
@@ -52,27 +70,27 @@ namespace allscale { namespace data_item_manager {
         {
         }
 
-//         template<typename DataItemType>
-// 		void release(const allscale::lease<DataItemType>& lease)
+//         template<typename DataItem>
+// 		void release(const allscale::lease<DataItem>& lease)
 //         {
 // //             if (lease.mode == access_mode::Invalid)
 // //                 return;
-// //             data_item_manager_impl<DataItemType>::release(lease);
+// //             data_item_manager_impl<DataItem>::release(lease);
 //         }
 //
-//         template<typename DataItemType>
-// 		void release(const std::vector<allscale::lease<DataItemType>>& lease)
+//         template<typename DataItem>
+// 		void release(const std::vector<allscale::lease<DataItem>>& lease)
 //         {
 // //             for(auto const& l: lease)
 // //                 if (l.mode == access_mode::Invalid) return;
 // //
-// //             data_item_manager_impl<DataItemType>::release(lease);
+// //             data_item_manager_impl<DataItem>::release(lease);
 //         }
 
-        template<typename DataItemType>
-		void destroy(const data_item_reference<DataItemType>& ref)
+        template<typename DataItem>
+		void destroy(const data_item_reference<DataItem>& ref)
         {
-//             data_item_manager_impl<DataItemType>::destroy(ref);
+//             data_item_manager_impl<DataItem>::destroy(ref);
         }
 }}//end namespace allscale
 
